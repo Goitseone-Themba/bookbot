3916e96ed66f5ee5964e674281e1a825c04608d6
Goitseone-Themba <goitseonerozthemba@gmail.com>
2025-01-21 16:32:28 +0200

    print book and count words

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_content = f.read()
-        print(file_content)
+        words = file_content.split()
+        word_count = len(words)
+        print(word_count)
 
 main()
